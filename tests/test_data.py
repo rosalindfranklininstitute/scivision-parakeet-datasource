40300b4aebdb67553d67d33b13cd3d3d05dc40ac
James Parkhurst <james.parkhurst@diamond.ac.uk>
2022-11-09 09:17:48 +0000

    Skip test

--- tests/test_data.py
+++ tests/test_data.py
@@ -16,6 +16,10 @@ def test_parakeet_4V1W_1_particle_still(dataset):
 
 
 def test_parakeet_4V1W_100_particles_tilt_series(dataset):
+    
+    if os.getenv("CI"):
+        pytest.skip("Take's too long for github workflow")
+        return
 
     # Sim the images
     images = dataset.parakeet_4V1W_100_particles_tilt_series.read()
